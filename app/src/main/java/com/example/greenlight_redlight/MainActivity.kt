8a32cd5f55badedbe145fbde184da459637287f5
Noah Yonghyun Kim <yhkim6087@unist.ac.kr>
2021-12-03 10:25:28 +0900

    Develop1 Front Page

--- app/src/main/java/com/example/greenlight_redlight/MainActivity.kt
+++ app/src/main/java/com/example/greenlight_redlight/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.greenlight_redlight
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.greenlight_redlight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var NextButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        NextButton = binding.button
+
+        NextButton.setOnClickListener({
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent) // Transition to the next(MainActivity2) window
+            finish() // CLOSE current(MainActivity) window
+        })
+
     // Develop1
     }
 }
---
+++ app/src/main/java/com/example/greenlight_redlight/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.greenlight_redlight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
