c40a37aa13ef4c91c5f371a76aba8fb25cbb1c1c
Noah Yonghyun Kim <yhkim6087@unist.ac.kr>
2021-12-04 10:56:34 +0900

    Sensor Part

--- app/src/main/java/com/example/greenlight_redlight/FailActivity.kt
+++ app/src/main/java/com/example/greenlight_redlight/FailActivity.kt
@@ -1,32 +1,17 @@
 package com.example.greenlight_redlight
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.ImageButton
 import android.widget.Toast
-import com.example.greenlight_redlight.databinding.ActivityAppinfoBinding
 
-class AppinfoActivity : AppCompatActivity() {
-
-    private lateinit var BackButton: ImageButton
-
+class FailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_appinfo)
-
-        val binding = ActivityAppinfoBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        BackButton = binding.backButton
-
-        BackButton.setOnClickListener({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent) // Transition to the next(MainActivity2) window
-            finish() // CLOSE current(MainActivity) window
-        })
+        setContentView(R.layout.activity_fail)
     }
+
     // Back Press Caution Function //
     private var doubleBackToExit = false
     override fun onBackPressed() {
