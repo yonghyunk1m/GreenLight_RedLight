c3fba36b413e3bdeab1f8cd55da76548d908e12b
Noah Yonghyun Kim <yhkim6087@unist.ac.kr>
2021-12-17 18:30:48 +0900

    Tagger Screen Updates

--- app/src/main/java/com/example/greenlight_redlight/RoomActivity.kt
+++ app/src/main/java/com/example/greenlight_redlight/RoomActivity.kt
@@ -12,6 +12,8 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
+import com.example.greenlight_redlight.databinding.ActivityRoomBinding
+
 
 class RoomActivity : AppCompatActivity() {
     lateinit var listView: ListView;
